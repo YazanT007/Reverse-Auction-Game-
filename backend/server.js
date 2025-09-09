2a7ce0f6d42585d3591be1512a17f1034a83150a
YazanT007 <yazantaha1@gmail.com>
2025-09-09 14:30:18 +1000

    Changed the server connection to MONGODB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const gameRoutes = require('./routes/gameRoutes');
 
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/games', gameRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/reverseAuctionGame', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('MongoDB connected');
+    console.log('MongoDB Atlas connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => console.error(err));
+
+console.log("MONGO_URI from env:", process.env.MONGO_URI);
+
 
